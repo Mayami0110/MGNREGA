c37af7e8995bab88197cdd135f3f55bccadff585
unknown <mayami0110@gmail.com>
2020-05-13 02:25:25 +0530

    MGNREGA-MIS Reports

---
+++ src/main/java/com/qa/mgnrega/pages/WebPortalPage.java
@@ -0,0 +1,41 @@
+package com.qa.mgnrega.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.mgnrega.base.TestBase;
+
+public class WebPortalPage extends TestBase {
+
+	@FindBy(xpath = "//*[@id=\"myslidemenu\"]/ul/li/a[contains(text(),'MIS Reports')]")
+	////*[@id="myslidemenu"]/ul/li/a[contains(text(),'MIS Reports')]
+	WebElement linkmisreports;
+
+	
+	public WebPortalPage() {
+		
+		PageFactory.initElements(driver, this);
+
+	}
+
+	// Actions
+	public String validateWebPortalPageTitle() {
+		
+		
+		
+		System.out.println(driver.getTitle());
+		
+		return driver.getTitle();
+	}
+
+	public MISReportsPage doClickOnMISReports() {
+
+		linkmisreports.click();
+		
+		return new MISReportsPage();
+
+	}
+
+}
+	
